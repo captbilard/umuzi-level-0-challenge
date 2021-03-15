da3b552e5e2b17eb65d2ab6b49478d3bb2cccec2
captbilard <bilardosinulu@gmail.com>
2021-03-15 22:37:04 +0100

    Task 0.2 commit

---
+++ task 0.2.js
@@ -0,0 +1,9 @@
+let x = 1 + 1 * 2
+let y = (1 + 1) * 2
+let z = 1 + ( 1 * 2 )
+let a = 1 + 1 * 2 / 2
+let b = (1 + 1 * 2 ) /  2
+
+console.log(
+    `x = ${x}, y = ${y}, z = ${z}, a = ${a}, b = ${b}`
+)
